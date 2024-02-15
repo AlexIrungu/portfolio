404f5e751edb96678934d266c2a571ee3928056e
AlexIrungu <aleaxmuiruri@gmail.com>
2024-02-15 15:01:19 +0300

    changed navbar

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@ import ScrollSpy from 'bootstrap/dist/js/bootstrap.bundle.min';
 
 // Function to initialize Bootstrap scrollspy
 function initializeScrollSpy() {
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
+    const navbar = document.querySelector('.navbar');
+    if (navbar) {
         new ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
+            target: '.navbar',
+            offset: 50,
         });
     }
 }
 
 // Function to collapse responsive navbar when toggler is visible
 function collapseNavbarOnItemClick() {
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = document.querySelectorAll('#navbarResponsive .nav-link');
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const responsiveNavItems = document.querySelectorAll('.navbar-nav .nav-link');
 
     responsiveNavItems.forEach(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
